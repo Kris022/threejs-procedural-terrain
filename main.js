ad68357a73b7dffbe311bbfc4e97b1c132f2ac84
Kris <kharonek@yahoo.com>
2022-11-03 10:24:03 +0000

    selecting individual verts

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 import { createNoise2D } from "simplex-noise";
+import { WireframeGeometry } from "three";
 
 const camera = new THREE.PerspectiveCamera(
   70,
@@ -17,59 +18,33 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
   canvas: document.querySelector("#bg"),
 });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animation);
 document.body.appendChild(renderer.domElement);
 
 // Helpers
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
-//const gridHelper = new THREE.GridHelper(10, 20); // size, divisions
-//scene.add(gridHelper);
+const planeGeometry = new THREE.PlaneGeometry( 1, 1 );
+const planeMaterial = new THREE.MeshNormalMaterial();
+const basePlane = new THREE.Mesh( planeGeometry, planeMaterial );
+scene.add( basePlane );
 
-// Land
-let geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-const material = new THREE.MeshNormalMaterial();
+basePlane.material.side = THREE.DoubleSide;
+basePlane.rotateX( -Math.PI / 2 );
 
-let cubes = [];
-
-const noise2D = createNoise2D();
-
-for (let i = -15; i < 15; i++) {
-  for (let j = -15; j < 15; j++) {
-    geometry.translate(0, 0.01, 0)
-    cubes[i] = new THREE.Mesh(geometry, material);
-
-    //let noise = Math.pow(noise2D * 10, i);
-    let value2d = noise2D(i, j);
-    let cubeSize = value2d * 5;
-    if (cubeSize == 0){
-      cubeSize = 1;
-    }
-    cubes[i].scale.y = cubeSize;//Math.pow(value2d, 1.5);
-    
-    //console.log(Math.pow(value2d, 1.5))
-
-    cubes[i].position.x = i * 0.5;
-    cubes[i].position.z = j * 0.5;
-    scene.add(cubes[i]);
-  }
+let verts = basePlane.geometry.attributes.position.array;
+verts[2] = -2;
+for (let i=0; i < verts.length; i=i+3){
+  console.log(verts[i], verts[i+1], verts[i+2])
 }
 
-function makeCube(cubes, position, height) {
-  const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-  const material = new THREE.MeshNormalMaterial();
+camera.position.z = 1.5;
 
-
-  for (let x=0; x < height; x++) {
-    let c = new THREE.Mesh(geometry, material);
-    c.position.y = x * 0.5;
-  }
-
-}
-
-// animation
-function animation(time) {
+function animate() {
+  requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
+
+animate();

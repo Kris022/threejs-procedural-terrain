6d3f5d5483d8bdba9ae365643188308ce4fe7335
Kris <kharonek@yahoo.com>
2022-11-18 23:02:40 +0000

    changing z position of random verts

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ const camera = new THREE.PerspectiveCamera(
   70,
   window.innerWidth / window.innerHeight,
   0.01,
-  100
+  1000
 );
-camera.position.z = 10;
+
+camera.position.z = -1.5;
 
 const scene = new THREE.Scene();
 
@@ -26,21 +27,41 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
-const planeGeometry = new THREE.PlaneGeometry( 1, 1 );
+const planeGeometry = new THREE.PlaneGeometry(2000, 2000, 256, 256);
+
 const planeMaterial = new THREE.MeshNormalMaterial();
-const basePlane = new THREE.Mesh( planeGeometry, planeMaterial );
-scene.add( basePlane );
+const basePlane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(basePlane);
 
 basePlane.material.side = THREE.DoubleSide;
-basePlane.rotateX( -Math.PI / 2 );
+basePlane.rotateX(Math.PI / 2);
 
-let verts = basePlane.geometry.attributes.position.array;
-verts[2] = -2;
-for (let i=0; i < verts.length; i=i+3){
-  console.log(verts[i], verts[i+1], verts[i+2])
+function modifyVertsSimple() {
+  let verts = basePlane.geometry.attributes.position.array; // get vertecies of plane
+
+  verts[2] = 2;
+
+  for (let i = 0; i < verts.length; i = i + 3) {
+    console.log(verts[i], verts[i + 1], verts[i + 2]);
+  }
 }
 
-camera.position.z = 1.5;
+function modifyVerts() {
+  // get all vertecies of the object
+  let verts = basePlane.geometry.attributes.position.array;
+
+  const peak = 60;
+
+  // pos.z = i + 2
+  for (let i = 0; i < verts.length; i = i + 3) {
+    verts[i+2] = peak * Math.random();
+  }
+
+  basePlane.geometry.attributes.position.needsUpdate = true;
+  basePlane.geometry.computeVertexNormals();
+}
+
+modifyVerts();
 
 function animate() {
   requestAnimationFrame(animate);

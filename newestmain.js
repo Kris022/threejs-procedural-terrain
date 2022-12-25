97e6a36004ebef49836d7fa811c1a24d987b519c
Kris <kharonek@yahoo.com>
2022-12-25 19:21:47 +0000

    Applied noise map with falloffmap to plane geometry

--- newestmain.js
+++ newestmain.js
@@ -1,37 +1,22 @@
 import { createNoise2D } from "simplex-noise";
 import alea from "alea";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';1
 
 const canvas = document.getElementById("noise-preview");
 
-function drawNoise(canvas, noise) {
-  const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  for (let i = 0; i < canvas.width; i++) {
-    for (let j = 0; j < canvas.width; j++) {
-      //  let val = noise[i][j] * 100;
-      let val = noise[i][j] * 225;
-
-      //	ctx.fillStyle = `hsl(136, 0%,${val}%)`;
-      ctx.fillStyle = `rgb(${val}, ${val},${val})`;
-      ctx.fillRect(i, j, 1, 1);
-    }
-  }
-}
-
-let gen = createNoise2D();
-
+let gen = createNoise2D(alea(""));
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 function noise(nx, ny) {
   // Rescale from -1.0:+1.0 to 0.0:1.0
   return gen(nx, ny) / 2 + 0.5;
 }
 
 function octave(nx, ny, octaves) {
-  let val = 0.25; // final noise value
-  let freq = 5; // Scale/Zoom value
+  let val = 0.15; // softness
+  let freq = 2.75; // Scale/Zoom value/mountains
   let max = 1; // divisor
-  let amp = 2;
+  let amp = 1;
 
   for (let i = 0; i < octaves; i++) {
     val += noise(nx * freq, ny * freq) * amp;
@@ -42,9 +27,6 @@ function octave(nx, ny, octaves) {
 
   return val / max;
 }
-
-const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
-
 
 function createNoiseMap(falloff) {
   let offsetX = 0.0,
@@ -95,9 +77,115 @@ function createFllOffMap() {
   }
   return map;
 }
-const falloffMap = createFllOffMap();
-const noiseMap = createNoiseMap(falloffMap);
-drawNoise(canvas, noiseMap);
+
+function drawNoise(canvas, noise) {
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < canvas.width; i++) {
+    for (let j = 0; j < canvas.width; j++) {
+      //  let val = noise[i][j] * 100;
+      let val = noise[i][j] * 225;
+
+      //	ctx.fillStyle = `hsl(136, 0%,${val}%)`;
+      ctx.fillStyle = `rgb(${val}, ${val},${val})`;
+      ctx.fillRect(i, j, 1, 1);
+    }
+  }
+
+  return ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+}
+
+function map(val, smin, smax, emin, emax) {
+	const t = (val - smin) / (smax - smin);
+	return (emax - emin) * t + emin;
+}
+
+function applyNoiseToMesh(mesh, data) {
+  const verts = mesh.geometry.attributes.position.array;
+  let vertIndex = 0;
+  
+	for (let j = 0; j < data.height; j++) {
+		for (let i = 0; i < data.width; i++) {
+			const n = j * data.height + i;
+			const col = data.data[n * 4]; // the red channel
+			let newZ = map(col, 0, 255, -10, 10); // map from 0:255 to -10:10 and set to vertez z
+
+			if (newZ > 2.5) newZ *= 1.3; // exaggerate the peaks
+
+			verts[vertIndex + 2] = newZ;
+			vertIndex += 3;
+		}
+	}
+}
+
+// Scene 3D
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
+camera.position.y = 150;
+
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer({
+	antialias: true,
+	canvas: document.querySelector('#sceneView'),
+});
+
+renderer.shadowMap.enabled = true;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animation);
+document.body.appendChild(renderer.domElement);
+
+scene.background = new THREE.Color(0x70a4cc);
+
+
+// Data
+const noiseMap = createNoiseMap(createFllOffMap());
+const data = drawNoise(canvas, noiseMap);
+
+// Plane Geometry
+const geo = new THREE.PlaneGeometry(data.width, data.height, data.width - 1, data.height - 1);
+const mat = new THREE.MeshPhongMaterial( {
+  wireframe: false,
+  flatShading: false,
+});
+
+const terrain = new THREE.Mesh( geo, mat );
+
+
+applyNoiseToMesh(terrain, data);
+
+terrain.geometry.computeVertexNormals();
+terrain.receiveShadow = true;
+terrain.castShadow = true;
+
+terrain.rotateX((-Math.PI) / 2);
+scene.add(terrain);
+
+
+
+
+// Helpers
+const controls = new OrbitControls(camera, renderer.domElement); // new FirstPersonControls(camera, renderer.domElement); // new OrbitControls( camera, renderer.domElement ); // new FlyControls( camera, renderer.domElement );
+controls.update();
+const gridHelper = new THREE.GridHelper(100, 10); // size, divisions
+
+const light = new THREE.DirectionalLight(0xffffff, 0.8);
+light.position.y = 50;
+light.position.x = 20;
+light.rotateX(15);
+light.castShadow = true;
+scene.add(light);
+
+const helper = new THREE.DirectionalLightHelper(light, 10);
+scene.add(helper);
+
+// animation
+function animation(time) {
+	renderer.render(scene, camera);
+}
 
 /*
 // Map values to range [0.0, 1.0]

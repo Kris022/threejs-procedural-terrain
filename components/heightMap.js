d0bdf08246b7de62764388f870a6c0bc85505a29
Kris <kharonek@yahoo.com>
2022-12-28 18:07:49 +0000

    Added terrain chunk class

--- components/heightMap.js
+++ components/heightMap.js
@@ -1,203 +1,204 @@
-import { createNoise2D } from "simplex-noise";
-import alea from "alea";
+import { createNoise2D } from 'simplex-noise';
+import alea from 'alea';
 
 export default class HeightMap {
-  constructor(width, height, seed="") {
-    this.width = width;
-    this.height = height;
-    
-    this.seed = seed;
-    this.noiseGen = createNoise2D();
-    if (seed != "") {
-        this.noiseGen = createNoise2D(alea(this.seed));
-    }
+	constructor(width, height, seed = '') {
+		this.width = width;
+		this.height = height;
 
-    this.freq = 3; // Scale/Zoom value/mountains
-    this.octaves = 3;
+		this.seed = seed;
+		this.noiseGen = createNoise2D();
+		if (seed != '') {
+			this.noiseGen = createNoise2D(alea(this.seed));
+		}
 
-    this.val = 0.15; // softness
-    this.max = 1; // divisor
-    this.amp = 1;
-    this.offsetX = 4;
-    this.offsetY = 0;
+		this.freq = 3; // Scale/Zoom value/mountains
+		this.octaves = 3;
 
-    this.applyFalloff = true;
-    this.falloffMap = [];
-  }
+		this.val = 0.15; // softness
+		this.max = 1; // divisor
+		this.amp = 1;
+		this.offsetX = 4;
+		this.offsetY = 0;
 
-  clamp(num, min, max) {
-    return Math.min(Math.max(num, min), max);
-  }
+		this.applyFalloff = true;
+		this.falloffMap = [];
+	}
 
-  noise(nx, ny) {
-    // Rescale from -1.0:+1.0 to 0.0:1.0
-    return this.noiseGen(nx, ny) / 2 + 0.5;
-  } // end of noise
+	clamp(num, min, max) {
+		return Math.min(Math.max(num, min), max);
+	}
 
-  octave(nx, ny, octaves) {
-    let val = this.val; // softness
-    let freq = this.freq; // Scale/Zoom value/mountains
-    let max = this.max; // divisor
-    let amp = this.amp;
+	noise(nx, ny) {
+		// Rescale from -1.0:+1.0 to 0.0:1.0
+		return this.noiseGen(nx, ny) / 2 + 0.5;
+	} // end of noise
 
-    for (let i = 0; i < octaves; i++) {
-      val += this.noise(nx * freq, ny * freq) * amp;
-      max += amp;
-      amp /= 2;
-      freq *= 2;
-    }
+	octave(nx, ny, octaves) {
+		let val = this.val; // softness
+		let freq = this.freq; // Scale/Zoom value/mountains
+		let max = this.max; // divisor
+		let amp = this.amp;
 
-    return val / max;
-  } // end of octave
+		for (let i = 0; i < octaves; i++) {
+			val += this.noise(nx * freq, ny * freq) * amp;
+			max += amp;
+			amp /= 2;
+			freq *= 2;
+		}
 
-  generateNoiseMap() {
-    // check if falloff map is to be applied and create it if doesnt exist
-    if (this.applyFalloff && this.falloffMap.length == 0) {
-      //this.falloffMap = this.createFalloffMap();
-      this.falloffMap = this.gradientFalloff();
-    }
+		return val / max;
+	} // end of octave
 
-    // height map to be returned
-    let heightMap = [];
+	generateNoiseMap() {
+		// check if falloff map is to be applied and create it if doesnt exist
+		if (this.applyFalloff && this.falloffMap.length == 0) {
+			//this.falloffMap = this.createFalloffMap();
+			this.falloffMap = this.gradientFalloff();
+		}
 
-    for (let y = 0; y < this.height; y++) {
-      heightMap[y] = []; // insert row
-      for (let x = 0; x < this.width; x++) {
-        let nx = x / this.width - 0.5 + this.offsetY;
-        let ny = y / this.height - 0.5 + this.offsetX;
+		// height map to be returned
+		let heightMap = [];
 
-        let e = this.octave(nx, ny, this.octaves); // Compute octaves
+		for (let y = 0; y < this.height; y++) {
+			heightMap[y] = []; // insert row
+			for (let x = 0; x < this.width; x++) {
+				let nx = x / this.width - 0.5 + this.offsetY;
+				let ny = y / this.height - 0.5 + this.offsetX;
 
-        if (this.applyFalloff) {
-          e = this.clamp(e - this.falloffMap[y][x], 0, 1);
-        }
-        //  e = Math.pow(e, 2);
+				let e = this.octave(nx, ny, this.octaves); // Compute octaves
 
-        heightMap[y][x] = e; // Add elevation value to the array
-      }
-    }
+				if (this.applyFalloff) {
+					e = this.clamp(e - this.falloffMap[y][x], 0, 1);
+				}
+				//  e = Math.pow(e, 2);
 
-    return heightMap;
-  } // end of generateNoiseMap
+				heightMap[y][x] = e; // Add elevation value to the array
+			}
+		}
 
-  createFalloffMap() {
-    const a = 1;
-    const b = 5;
+		return heightMap;
+	} // end of generateNoiseMap
 
-    let map = [];
-    for (let y = 0; y < this.height; y++) {
-      map[y] = [];
-      for (let x = 0; x < this.width; x++) {
-        let sampleX = (x / this.height) * 2 - 1; // get sample in range [-1, 1]
-        let sampleY = (y / this.height) * 2 - 1;
-        let val = Math.max(Math.abs(sampleX), Math.abs(sampleY));
-        val = Math.pow(val, a) / (Math.pow(val, a) + Math.pow(b - b * val, a));
-        console.log(val);
-        map[y][x] = val;
-      }
-    }
-    return map;
-  } // end of create falloffMap
+	createFalloffMap() {
+		const a = 1;
+		const b = 5;
 
-  mapRange(value, inMin, inMax, outMin, outMax) {
-    // Calculate the proportion of the value within the input range
-    const proportion = (value - inMin) / (inMax - inMin);
+		let map = [];
+		for (let y = 0; y < this.height; y++) {
+			map[y] = [];
+			for (let x = 0; x < this.width; x++) {
+				let sampleX = x / this.height * 2 - 1; // get sample in range [-1, 1]
+				let sampleY = y / this.height * 2 - 1;
+				let val = Math.max(Math.abs(sampleX), Math.abs(sampleY));
+				val = Math.pow(val, a) / (Math.pow(val, a) + Math.pow(b - b * val, a));
+				console.log(val);
+				map[y][x] = val;
+			}
+		}
+		return map;
+	} // end of create falloffMap
 
-    // Map the value to the output range using the calculated proportion
-    return outMin + (outMax - outMin) * proportion;
-  }
+	mapRange(value, inMin, inMax, outMin, outMax) {
+		// Calculate the proportion of the value within the input range
+		const proportion = (value - inMin) / (inMax - inMin);
 
-  gradientFalloff() {
-    let intensity = 200; // color intensity
-    // get canvas
-    const canvas = document.getElementById("color-preview");
-    const ctx = canvas.getContext("2d");
+		// Map the value to the output range using the calculated proportion
+		return outMin + (outMax - outMin) * proportion;
+	}
 
-    // cetner point
-    let x2 = canvas.width / 2;
-    let y2 = canvas.height / 2;
+	gradientFalloff() {
+		let intensity = 200; // color intensity
+		// get canvas
+		const canvas = document.getElementById('color-preview');
+		const ctx = canvas.getContext('2d');
 
-    // draw gradient
-    let map = [];
-    for (let y = 0; y < this.height; y++) {
-      map[y] = [];
-      for (let x = 0; x < this.width; x++) {
-        let a = x - x2;
-        let b = y - y2;
+		// cetner point
+		let x2 = canvas.width / 2;
+		let y2 = canvas.height / 2;
 
-        let dist = Math.sqrt(a * a + b * b);
-        let c = (dist * 255 / 135);
-        c = this.mapRange(c, 0, 255, 0, 1);
-        map[y][x] = c;
-         /*  
+		// draw gradient
+		let map = [];
+		for (let y = 0; y < this.height; y++) {
+			map[y] = [];
+			for (let x = 0; x < this.width; x++) {
+				let a = x - x2;
+				let b = y - y2;
+
+				let dist = Math.sqrt(a * a + b * b);
+				let c = dist * 255 / 135;
+				c = this.mapRange(c, 0, 255, 0, 1);
+				map[y][x] = c;
+				/*
         ctx.fillStyle = `rgb(${c},${c},${c})`;
         ctx.fillRect(x, y, 1, 1);
         */
-      }
-    }
+			}
+		}
 
-    // return data
-    return map;
-  }
+		// return data
+		return map;
+	}
 
-  drawColorMap(canvas, noise) {
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+	drawColorMap(canvas, noise) {
+		const ctx = canvas.getContext('2d');
+    // clear the canvas
+		ctx.fillStyle = 'white';
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Biome layers
-    const water = "rgb(98, 154, 245)";
-    const sand1 = "rgb(255, 247, 204)";
-    const sand2 = "rgb(255, 235, 130)";
-    const sand3 = "rgb(245, 218, 66)";
-    const grass1 = "rgb(205, 250, 137)";
-    const grass2 = "rgb(181, 245, 86)";
-    const rock1 = "rgb(153, 153, 153)";
-    const rock2 = "rgb(138, 138, 138)";
-    const snow = "rgb(224, 224, 224)";
+		// Biome layers
+		const water = 'rgb(98, 154, 245)';
+		const sand1 = 'rgb(255, 247, 204)';
+		const sand2 = 'rgb(255, 235, 130)';
+		const sand3 = 'rgb(245, 218, 66)';
+		const grass1 = 'rgb(205, 250, 137)';
+		const grass2 = 'rgb(181, 245, 86)';
+		const rock1 = 'rgb(153, 153, 153)';
+		const rock2 = 'rgb(138, 138, 138)';
+		const snow = 'rgb(224, 224, 224)';
 
-    for (let i = 0; i < canvas.width; i++) {
-      for (let j = 0; j < canvas.width; j++) {
-        let val = noise[i][j];
-        if (val < 0.002) {
-          ctx.fillStyle = water;
-        } else if (val < 0.03) {
-          ctx.fillStyle = sand1;
-        } else if (val < 0.08) {
-          ctx.fillStyle = sand2;
-        } else if (val < 0.1) {
-          ctx.fillStyle = sand3;
-        } else if (val < 0.15) {
-          ctx.fillStyle = grass1;
-        } else if (val < 0.3) {
-          ctx.fillStyle = grass2;
-        } else if (val < 0.35) {
-          ctx.fillStyle = rock1;
-        } else if (val < 0.45) {
-          ctx.fillStyle = rock2;
-        } else {
-          ctx.fillStyle = snow;
-        }
-        ctx.fillRect(i, j, 1, 1);
-      }
-    }
+		for (let i = 0; i < canvas.width; i++) {
+			for (let j = 0; j < canvas.width; j++) {
+				let val = noise[i][j];
+				if (val < 0.002) {
+					ctx.fillStyle = water;
+				} else if (val < 0.03) {
+					ctx.fillStyle = sand1;
+				} else if (val < 0.08) {
+					ctx.fillStyle = sand2;
+				} else if (val < 0.1) {
+					ctx.fillStyle = sand3;
+				} else if (val < 0.15) {
+					ctx.fillStyle = grass1;
+				} else if (val < 0.3) {
+					ctx.fillStyle = grass2;
+				} else if (val < 0.35) {
+					ctx.fillStyle = rock1;
+				} else if (val < 0.45) {
+					ctx.fillStyle = rock2;
+				} else {
+					ctx.fillStyle = snow;
+				}
+				ctx.fillRect(i, j, 1, 1);
+			}
+		}
 
-    return ctx.getImageData(0, 0, canvas.width, canvas.height);
-  }
+		return ctx.getImageData(0, 0, canvas.width, canvas.height);
+	}
 
-  drawNoise(canvas, noise) {
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+	drawNoise(canvas, noise) {
+		const ctx = canvas.getContext('2d');
+		ctx.fillStyle = 'white';
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < canvas.width; i++) {
-      for (let j = 0; j < canvas.width; j++) {
-        let val = noise[i][j] * 225;
-        ctx.fillStyle = `rgb(${val}, ${val},${val})`;
-        ctx.fillRect(i, j, 1, 1);
-      }
-    }
+		for (let i = 0; i < canvas.width; i++) {
+			for (let j = 0; j < canvas.width; j++) {
+				let val = noise[i][j] * 225;
+				ctx.fillStyle = `rgb(${val}, ${val},${val})`;
+				ctx.fillRect(i, j, 1, 1);
+			}
+		}
 
-    return ctx.getImageData(0, 0, canvas.width, canvas.height);
-  }
+		return ctx.getImageData(0, 0, canvas.width, canvas.height);
+	}
 } // end of class
--- main2.js
+++ main2.js
@@ -1,20 +1,21 @@
-import HeightMap from "./components/heightMap";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import HeightMap from './components/heightMap';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Sky } from 'three/examples/jsm/objects/Sky.js';
+import { Water } from 'three/examples/jsm/objects/Water.js';
 
 // Get canvases
-const noiseCanvas = document.getElementById("noise-preview");
-const colorCanvas = document.getElementById("color-preview");
+const noiseCanvas = document.getElementById('noise-preview');
+const colorCanvas = document.getElementById('color-preview');
 
 // Object for generating map data
 const mapGenerator = new HeightMap(noiseCanvas.width, noiseCanvas.height);
-const heightMap = mapGenerator.generateNoiseMap();  // height map
+const heightMap = mapGenerator.generateNoiseMap(); // height map
 
-const d = mapGenerator.drawNoise(noiseCanvas, heightMap);
+const d = mapGenerator.drawNoise(noiseCanvas, heightMap); //
 mapGenerator.drawColorMap(colorCanvas, heightMap);
 
 const f = mapGenerator.gradientFalloff();
-
 
 function map(val, smin, smax, emin, emax) {
 	const t = (val - smin) / (smax - smin);
@@ -22,43 +23,37 @@ function map(val, smin, smax, emin, emax) {
 }
 
 function applyNoiseToMesh(mesh, data) {
-    const verts = mesh.geometry.attributes.position.array;
-    let vertIndex = 0;
-    const heightMultiplier = 1.25; // 
-    
-      for (let j = 0; j < data.height; j++) {
-          for (let i = 0; i < data.width; i++) {
-              const n = j * data.height + i;
-              const col = data.data[n * 4]; // the red channel
-              let newZ = map(col, 0, 255, -10, 10); // map from 0:255 to -10:10 and set to vertez z
+	const verts = mesh.geometry.attributes.position.array; // get verticies of geometry
+	let vertIndex = 0; // current vertex index
+	const heightMultiplier = 1.25; //
 
-              if (newZ < -1) newZ *= heightMultiplier; // exaggerate the peaks
-              //newZ *= heightMultiplier; // exaggerate the peaks
-        
-              verts[vertIndex + 2] = newZ;
-              vertIndex += 3;
-          }
-      }
-  
-    // restore mesh position to origin 
-   // mesh.position.y += heightMultiplier;
-  
-  }
+	for (let j = 0; j < data.height; j++) {
+		for (let i = 0; i < data.width; i++) {
+			const n = j * data.height + i;
+			const col = data.data[n * 4]; // the red channel
+			let newZ = map(col, 0, 255, -10, 10); // map from 0:255 to -10:10 and set to vertez z
+			//if (newZ < -1) newZ *= heightMultiplier; // exaggerate the peaks
+
+			//if (newZ != -10) newZ *= 1.5; //
+		//	if (newZ <= -10) newZ *= 1.05; //
+
+			//newZ *= heightMultiplier; // exaggerate the peaks
+
+			verts[vertIndex + 2] = newZ;
+			vertIndex += 3;
+		}
+	}
+	// restore mesh position to origin
+	 //mesh.position.y += 10;
+}
 
 //-------------------------- Scene 3D --------------------------\\
-const camera = new THREE.PerspectiveCamera(
-  70,
-  window.innerWidth / window.innerHeight,
-  0.01,
-  1000
-);
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
 camera.position.y = 150;
 
-const scene = new THREE.Scene();
-
 const renderer = new THREE.WebGLRenderer({
-  antialias: true,
-  canvas: document.querySelector("#sceneView"),
+	antialias: true,
+	canvas: document.querySelector('#sceneView'),
 });
 
 renderer.shadowMap.enabled = true;
@@ -67,19 +62,18 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animation);
 document.body.appendChild(renderer.domElement);
 
+const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x70a4cc);
 
 
-const terrainGeo = new THREE.PlaneGeometry(
-  100,100,
-  noiseCanvas.width - 1,
-  noiseCanvas.height - 1
-);
+
+// ----------------------- Terrain ----------------------- \\
+const terrainGeo = new THREE.PlaneGeometry(100, 100, noiseCanvas.width - 1, noiseCanvas.height - 1);
 const canvasTexture = new THREE.CanvasTexture(colorCanvas);
 const terrainMat = new THREE.MeshPhongMaterial({
-  map: canvasTexture,
-  wireframe: false,
-  flatShading: true,
+	map: canvasTexture,
+	wireframe: false,
+	flatShading: true,
 });
 
 const terrain = new THREE.Mesh(terrainGeo, terrainMat);
@@ -90,11 +84,11 @@ terrain.geometry.computeVertexNormals();
 terrain.receiveShadow = true;
 terrain.castShadow = true;
 
-terrain.rotateX(-Math.PI / 2);
+terrain.rotateX((-Math.PI) / 2);
 scene.add(terrain);
 
 // Helpers
-const controls = new OrbitControls(camera, renderer.domElement); // new FirstPersonControls(camera, renderer.domElement); // new OrbitControls( camera, renderer.domElement ); // new FlyControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement); //
 controls.update();
 const gridHelper = new THREE.GridHelper(100, 10); // size, divisions
 
@@ -106,9 +100,9 @@ light.castShadow = true;
 scene.add(light);
 
 const helper = new THREE.DirectionalLightHelper(light, 10);
-scene.add(helper);
+//scene.add(helper);
 
 // animation
 function animation(time) {
-  renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
